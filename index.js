a7c5d994651fe6489d292d6ee1cecaa89db5aafb
y5ni <yuritsui0@gmail.com>
2026-05-22 19:43:47 +0300

    Implement XP gain and level up notifications
    Add user XP and leveling system to message handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 
 const client = new Client({
   intents: [
@@ -17,7 +18,45 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
+  if (message.author.bot) return;
+
+  let user = await User.findOne({
+    userId: message.author.id,
+    guildId: message.guild.id
+  });
+
+  if (!user) {
+    user = new User({
+      userId: message.author.id,
+      guildId: message.guild.id,
+      xp: 0,
+      level: 1
+    });
+  }
+
+  const randomXP = Math.floor(Math.random() * 10) + 5;
+  user.xp += randomXP;
+
+  const neededXP = user.level * 100;
+
+  if (user.xp >= neededXP) {
+    user.level += 1;
+    user.xp = 0;
+
+    message.channel.send(
+      `🎉 | ${message.author} وصل لفل ${user.level}!`
+    );
+  }
+
+  await user.save();
+
+  if (message.content === '!rank') {
+    message.reply(
+      `لفلك: ${user.level} | XP: ${user.xp}/${neededXP}`
+    );
+  }
+
   if (message.content === '!ping') {
     message.reply('pong 🏓');
   }
