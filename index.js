5dd637ea858e0148982b7287e548a251c4b93647
y5ni <yuritsui0@gmail.com>
2026-05-23 15:19:03 +0300

    Add voice channel join command and update intents

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
   ]
 });
 
@@ -12,11 +14,28 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
   if (message.author.bot) return;
 
+  // أمر Ping
   if (message.content.toLowerCase() === '!ping') {
-    message.reply('pong 🏓');
+    return message.reply('pong 🏓');
+  }
+
+  // أمر دخول الفويس
+  if (message.content.toLowerCase() === '!join') {
+
+    if (!message.member.voice.channel) {
+      return message.reply('ادخل روم فويس أول 😭');
+    }
+
+    joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator
+    });
+
+    return message.reply('دخلت الفويس 😼🎧');
   }
 });
 
