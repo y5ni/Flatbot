053e2ac509c4dac5af4ed6560b187ae9bd721641
y5ni <yuritsui0@gmail.com>
2026-05-23 14:58:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const mongoose = require('mongoose');
-const User = require('./models/User');
 
 const client = new Client({
   intents: [
@@ -10,56 +8,15 @@ const client = new Client({
   ]
 });
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB Connected ✅'))
-.catch(err => console.log(err));
-
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', async message => {
+client.on('messageCreate', message => {
   if (message.author.bot) return;
-  if (!message.guild) return;
-
-  let user = await User.findOne({
-    userId: message.author.id,
-    guildId: message.guild.id
-  });
-
-  if (!user) {
-    user = new User({
-      userId: message.author.id,
-      guildId: message.guild.id,
-      xp: 0,
-      level: 1
-    });
-  }
-
-  const randomXP = Math.floor(Math.random() * 10) + 5;
-  user.xp += randomXP;
-
-  const neededXP = user.level * 100;
-
-  if (user.xp >= neededXP) {
-    user.level += 1;
-    user.xp = 0;
-
-    message.channel.send(
-      `🎉 | ${message.author} وصل لفل ${user.level}!`
-    );
-  }
-
-  await user.save();
-
-  if (message.content.toLowerCase() === '!rank') {
-    return message.reply(
-      `لفلك: ${user.level} | XP: ${user.xp}/${neededXP}`
-    );
-  }
 
   if (message.content.toLowerCase() === '!ping') {
-    return message.reply('pong 🏓');
+    message.reply('pong 🏓');
   }
 });
 
