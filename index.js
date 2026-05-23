39567d0f3337ac73aa54ccb1e60538686c623d15
y5ni <yuritsui0@gmail.com>
2026-05-23 15:41:40 +0300

    Add leave command for voice channel

--- index.js
+++ index.js
@@ -23,7 +23,18 @@ client.on('messageCreate', async message => {
 
   // أمر دخول الفويس
   if (message.content.toLowerCase() === '!join') {
+if (message.content.toLowerCase() === '!leave') {
 
+  const connection = getVoiceConnection(message.guild.id);
+
+  if (!connection) {
+    return message.reply('أنا مو بالفويس 😭');
+  }
+
+  connection.destroy();
+
+  return message.reply( ' 😔🤘🏻.');
+}
     if (!message.member.voice.channel) {
       return message.reply('ادخل روم فويس أول 😭');
     }
