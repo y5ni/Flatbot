6768ef65fd398261cc6ad4d67a58583cad8f1d4d
y5ni <yuritsui0@gmail.com>
2026-05-26 18:23:58 +0300

    Add required validation to userId and guildId

--- models/User.js
+++ models/User.js
@@ -1,17 +1,27 @@
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: String,
-  guildId: String,
+
+  userId: {
+    type: String,
+    required: true
+  },
+
+  guildId: {
+    type: String,
+    required: true
+  },
+
   xp: {
     type: Number,
     default: 0
   },
+
   level: {
     type: Number,
     default: 1
   }
+
 });
 
 module.exports = mongoose.model('User', userSchema);
