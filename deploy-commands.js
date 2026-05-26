c2f39cb95d16a22f078b288cfd7b8d15b2daf19b
y5ni <yuritsui0@gmail.com>
2026-05-26 21:38:05 +0300

    Add new slash commands for Discord bot

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,26 @@
 require('dotenv').config();
 
-const { REST, Routes } = require('discord.js');
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('pong'),
+
+  new SlashCommandBuilder()
+    .setName('join')
+    .setDescription('دخول الفويس'),
+
+  new SlashCommandBuilder()
+    .setName('leave')
+    .setDescription('الخروج من الفويس'),
+
+  new SlashCommandBuilder()
+    .setName('rank')
+    .setDescription('عرض لفلك')
+
+].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' })
 .setToken(process.env.TOKEN);
@@ -9,24 +29,17 @@ const rest = new REST({ version: '10' })
 
   try {
 
-    console.log('جاري حذف كل أوامر السلاش...');
+    console.log('جاري تسجيل أوامر السيرفر...');
 
-    // حذف أوامر GLOBAL
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: [] }
-    );
-
-    // حذف أوامر السيرفر
     await rest.put(
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
         '1492204029118386423'
       ),
-      { body: [] }
+      { body: commands }
     );
 
-    console.log('تم حذف كل الأوامر ✅');
+    console.log('تم تسجيل الأوامر بنجاح ✅');
 
   } catch (error) {
     console.error(error);
